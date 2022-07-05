cb36725d28e6e56ae3b6f66f6211d4362d2bfb2e
mmengi18 <mmengi18@montgomerycollege.edu>
2022-07-05 09:47:25 -0400

    used google and stackoverflow

--- exercises/js-logic.js
+++ exercises/js-logic.js
@@ -18,8 +18,18 @@ Ex: If the number passed in is 10; 3, 5, 6, and 9 are both less than 10 and mult
 */
 const sumOfNumbers = (num) => {
 	// answer here
+	// if num is less than 10 and and multiples of 3 or 5 
+	// return the sum of these numbers
+	let sum = 0; 
+	for (let i = 1; i < num; i++) {
+		if (i % 3 === 0 || i % 5 === 0 ) {
+			sum += i
+		}
+		
+	}
+	return sum;
 }
-
+console.log(sumOfNumbers(10))
 /*
 Question 2
 
@@ -27,6 +37,7 @@ Finish the function. Using the string that is passed in return the number of how
 */
 const vowelCount = (str) => {
 	// answer here
+	String
 }
 
 /*
