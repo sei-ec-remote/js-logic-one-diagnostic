4345688afe6e487e9aebbc54e03e82acc853a290
f-eiyu <feiyu@alum.mit.edu>
2022-07-05 09:15:52 -0400

    Finish diagnostic

--- exercises/js-logic.js
+++ exercises/js-logic.js
@@ -17,7 +17,12 @@ Finish the function. Using the number that is passed in, `num`, first find all t
 Ex: If the number passed in is 10; 3, 5, 6, and 9 are both less than 10 and multiples of 3 or 5. The sum of these numbers are 23
 */
 const sumOfNumbers = (num) => {
-	// answer here
+	const multiples = [];
+	for (let n = 1; n < num; n++) {
+		if (n % 3 === 0 || n % 5 === 0) { multiples.push(n); };
+	}
+
+	return multiples.reduce((acc, n) => { return acc + n; });
 }
 
 /*
@@ -26,7 +31,12 @@ Question 2
 Finish the function. Using the string that is passed in return the number of how many vowels occur.
 */
 const vowelCount = (str) => {
-	// answer here
+	let vowelCount = 0;
+	for (let ch of str) {
+		if ("aeiou".includes(ch)) { vowelCount++;}
+	}
+
+	return vowelCount;
 }
 
 /*
@@ -37,7 +47,13 @@ Finish the function. Using the string that is passed in return the same string b
 Ex: If the string that is passed in is `hey there person` the return value should be `hey ereht nosrep`
 */
 const stringReverse = (str) => {
-	// answer here
+	const strArr = str.split(" "); // certainly not the most general solution
+	strArr.forEach((word, i) => {
+		if (word.length < 5) { return; }
+		strArr[i] = word.split("").reverse().join("");
+	})
+
+	return strArr.join(" ");
 }
 
 module.exports = {
