d81ec39ff45eddbfec7bafc39f3c764e6734b289
lyndonna.munro@gmail.com <lyndonnamunro@Lyndonnas-Air.verizon.net>
2022-07-05 09:56:26 -0400

    Diagnostic Complete

--- exercises/js-logic.js
+++ exercises/js-logic.js
@@ -2,11 +2,11 @@
 =============== JavaScript Logic Challenge I ==================
 
 GOAL: Read each question and write code to complete each task
-      given. Do not change starter code or function names.
+	  given. Do not change starter code or function names.
 
 TEST: To test run the commands `npm run test` in your terminal
-      at the root of this directory.(js-logic-one-diagnostic)
-      Don't worry about capitalization.
+	  at the root of this directory.(js-logic-one-diagnostic)
+	  Don't worry about capitalization.
 */
 
 /*
@@ -17,7 +17,13 @@ Finish the function. Using the number that is passed in, `num`, first find all t
 Ex: If the number passed in is 10; 3, 5, 6, and 9 are both less than 10 and multiples of 3 or 5. The sum of these numbers are 23
 */
 const sumOfNumbers = (num) => {
-	// answer here
+	let sum = 0;
+	for (let i = 1; i < num; i++) {
+		if (i % 3 == 0 || i % 5 == 0) {
+			sum = sum + i
+		}
+	}
+	return sum;
 }
 
 /*
@@ -26,22 +32,38 @@ Question 2
 Finish the function. Using the string that is passed in return the number of how many vowels occur.
 */
 const vowelCount = (str) => {
-	// answer here
+	let count = 0;
+	const vowels = ['a', 'e', 'i', 'o', 'u']
+	for (let vowel of str.toLowerCase()) {
+		if (vowels.includes(vowel)) {
+			count++
+		}
+	} return count
 }
 
 /*
 Question 3
 
-Finish the function. Using the string that is passed in return the same string but if a word in that string is longer than 5 word reverse it.
+Finish the function. Using the string that is passed in return the same string but if a word in that string is longer than 5 letters reverse it.
 
 Ex: If the string that is passed in is `hey there person` the return value should be `hey ereht nosrep`
 */
 const stringReverse = (str) => {
-	// answer here
+	let word = str.split(' ')
+	let returnWord = []
+	word.forEach((i) => {
+		if (i.length >= 5) {
+			i = i.split('').reverse().join('')
+			returnWord.push(i)
+		} else {
+			returnWord.push(i)
+		}
+	})
+	return returnWord.join(' ')
 }
 
 module.exports = {
 	sumOfNumbers,
 	vowelCount,
-    stringReverse
+	stringReverse
 }
